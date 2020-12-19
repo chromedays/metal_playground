4b3b4b066ce14a70c7a37027fe49f399fcb215f1
Ilgwon Ha <haik0925@gmail.com>
2020-12-19 00:17:43 -0800

    fixed weird path issue on fastbuild + added unitybuild

--- build.sh
+++ build.sh
@@ -5,8 +5,12 @@ then
 trash bin
 fi
 mkdir bin
+
+./FBuild Exe-playground-Unity -clean -verbose -showcmds
+./FBuild Exe-playground -compdb
+
 pushd bin
-CLANG="clang"
+# CLANG="clang"
 OUTPUT="playground"
 # COMPILE_COMMAND="${CLANG} -g -c -std=c11 -o main.o ../src/main.m  -fobjc-arc -fdiagnostics-absolute-paths --target=arm64-apple-macos11"
 # LINK_COMMAND="${CLANG} -g main.o -o ${OUTPUT} -framework AppKit -framework MetalKit -framework Metal"
@@ -15,9 +19,6 @@ OUTPUT="playground"
 # eval "$COMPILE_COMMAND"
 # eval "$LINK_COMMAND"
 # eval "$CLANGD_COMMAND"
-./../FBuild -config ../fbuild.bff Exe-playground -clean -verbose -showcmds
-./../FBuild -config ../fbuild.bff Exe-playground -compdb
-
 xcrun -sdk macosx metal -c -gline-tables-only -MO ../src/shaders.metal -o shaders.air
 xcrun -sdk macosx metallib shaders.air -o shaders.metallib
 trash shaders.air
